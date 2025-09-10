1ad9c5598016fdcc6df10a817f3cbbab2b6cfb9d
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-09-10 21:03:09 +0500

    chore: base project

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "A beginning of an awesome project bootstrapped with github:bleur-org/templates";
+
+  inputs = {
+    # Stable for keeping thins clean
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+
+    # Fresh and new for testing
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    # The flake-utils library
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      # Nix script formatter
+      formatter = pkgs.alejandra;
+
+      # Development environment
+      devShells.default = import ./shell.nix {inherit pkgs fenix;};
+
+      # Output package
+      packages.default = pkgs.callPackage ./. {inherit pkgs fenix;};
+    })
+    // {
+      # NixOS module (deployment)
+      nixosModules.server = import ./module.nix self;
+    };
+}
