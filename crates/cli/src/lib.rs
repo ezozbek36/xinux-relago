431ddc0d2c6529d7d08af6b0a8432b518668042c
lambdajon <lambdajon42@gmail.com>
2026-01-06 14:43:52 +0500

    wip: initial cli parser

--- crates/cli/src/lib.rs
+++ crates/cli/src/lib.rs
@@ -18,3 +18,55 @@ pub struct Cli {
 pub enum Commands {
     Exec,
 }
+
+pub fn init() -> anyhow::Result<()> {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::from_default_env()
+                .add_directive(tracing::Level::INFO.into()),
+        )
+        .init();
+
+    let matches = command!() // requires `cargo` feature
+        .arg(Arg::new("exec").action(ArgAction::Append))
+        .get_matches();
+
+    let args = matches
+        .get_many::<String>("exec")
+        .unwrap_or_default()
+        .map(|v| v.as_str())
+        .collect::<Vec<_>>();
+
+    let sbcmd = args[1];
+    let _ = cmd_exec(sbcmd.clone());
+
+    let owned_words: Vec<&str> = sbcmd.split_whitespace().map(|x| x).collect();
+
+    Ok(())
+}
+
+fn cmd_exec(cmd: &str) -> anyhow::Result<()> {
+    let cm = Exec::shell(cmd);
+
+    match cm.clone().capture() {
+        Ok(x) => {
+            //   print!("FOWARDED COMMAND: {}", x.stderr_str());
+            // let v = cm.stream_stdout()?;
+            let v = cm.stream_stderr()?;
+            let mut reader = std::io::BufReader::new(v);
+            for line in reader.lines() {
+                match line {
+                    Ok(l) => {
+                        println!("Line :{}", l);
+                    }
+                    Err(e) => print!("Error:{}", e),
+                }
+            }
+        }
+        Err(e) => {
+            print!("{}", e)
+        }
+    }
+
+    Ok(())
+}
