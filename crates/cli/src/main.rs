c07e5a565ec5e5f9ea6cfb41c869a69e56c09310
lambdajon <lambdajon42@gmail.com>
2026-01-08 12:38:13 +0500

    wip: when runing application first runs cli

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -3,17 +3,15 @@
 use std::process::exit;
 
 use clap::Parser;
-use cli::{Cli, Commands};
+use cli::{run};
 // use utils::config::{Config, Field};
 
 fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt()
-        .with_env_filter(
-            tracing_subscriber::EnvFilter::from_default_env()
-                .add_directive(tracing::Level::INFO.into()),
-        )
-        .init();
-    print!("hello");
 
+    print!("hello");
+    
+    run();
+
+    
     Ok(())
 }
