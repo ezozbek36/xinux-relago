e31a4d35644eae6f24c61664ecfed6dd07c418b6
lambdajon <lambdajon42@gmail.com>
2026-01-26 17:02:10 +0500

    fix: setup flake parts

--- flake.nix
+++ flake.nix
@@ -1,47 +1,59 @@
 {
-  description = "Relago";
+  description = "Relago — bug reporter for Xinux";
 
   inputs = {
-    # Stable for keeping thins clean
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    # Too old to work with most libraries
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
-    # Fresh and new for testing
+    # Perfect!
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
+    # The flake-parts library
+    flake-parts.url = "github:hercules-ci/flake-parts";
     crane.url = "github:ipetkov/crane";
-
-    # The flake-utils library
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
-    crane,
     flake-utils,
     ...
   } @ inputs:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      craneLib = crane.mkLib pkgs;
-    in {
-      # Nix script formatter
-      formatter = pkgs.nixfmt-rfc-style;
-
-      # Development environment
-      devShells.default = import ./shell.nix {inherit self pkgs craneLib;};
-
-      # Output package
-      packages = {
-        default = self.packages.${system}.relago;
-        relago = pkgs.callPackage ./. {inherit pkgs craneLib;};
-        relago-dev = self.packages.${system}.relago.overrideAttrs {
-          dontCheck = true;
-        };
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} ({...}: {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      # pkgs = nixpkgs.legacyPackages.${system};
+      flake = {
+      	nixosModules.relago = import ./module.nix self;
       };
-    })
-    // {
-      # NixOS module (deployment)
-      nixosModules.relago = import ./module.nix self;
-    };
+      
+      perSystem = {
+        system,
+        ...
+      }: let 
+        pkgs = nixpkgs.legacyPackages.${system};
+        craneLib = inputs.crane.mkLib pkgs;
+        # slf = inputs.self;
+       in rec {
+
+        # Nix script formatter
+        formatter = pkgs.nixfmt-rfc-style;
+
+        # Development environment
+        devShells.default = import ./shell.nix  {inherit self pkgs craneLib;};
+
+        # Output package
+        # packages.default = pkgs.callPackage ./. {inherit pkgs;};
+        packages = {
+          default = self.packages.${system}.relago;
+          relago = pkgs.callPackage ./. {inherit pkgs craneLib;};
+          relago-dev = self.packages.${system}.relago.overrideAttrs {
+            dontCheck = true;
+          };
+      };
+		
+      };
+    });
 }
